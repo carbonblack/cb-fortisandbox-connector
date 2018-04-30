163ee5b3be02e2371ce9bb82d76ef1d3dc97dcf7
bit9qa <bit9qa@DevRel-Ansible.cbenglab.com>
2018-04-30 14:53:46 -0400

    UPdates to fortisandbox connector

--- cbopensource/connectors/fortisandbox/apiclient_fortisandbox.py
+++ cbopensource/connectors/fortisandbox/apiclient_fortisandbox.py
@@ -2,68 +2,94 @@
 
 import logging
 import os
+import api_fortisandbox
+from base64 import b64encode, b64decode
 
 log = logging.getLogger(__name__)
-
-class VTAPIQUOTAREACHED(Exception):
-    pass
 
 
 class FortiSandboxAnalysisClient(object):
 
-    def __init__(self, session=None, api_token=None, url=None,log_level=None):
+    def __init__(self, host, session=None, username=None,
+                 password=None, log_level=None):
         self.session = session
-        self.api_token = api_token
-        self.url = url if url else "https://www.fortisandbox.com/vtapi/v2/file/"
+        self.host = host
+	self.username = username
+	self.password = password
+	self._sid = None
         log.setLevel(log_level if log_level else logging.INFO)
 
+    @property
+    def sid(self):
+	while not(self._sid):
+		log.info("Trying to get new session ID")
+        	response = api_fortisandbox.handle_request(
+            	host=self.host,
+            	session=self.session,
+            	params={
+                "data":[{"user": self.username,
+                "passwd": self.password}]},
+            	request_type="login")
+		log.info(response)
+		log.info(response.json())
+		responsebody = response.json()
+		result = responsebody.get(u'result',None)
+		status = result.get(u'status',None) if result else None
+		if result and status: 
+		    self._sid = responsebody.get(u'session',None)
+		    message = status.get(u"message")
+		    code = status.get(u'code')
+		    
+		    if code == 0:
+	return self._sid
+
     def submit_file(self, resource_hash=None, stream=None):
-        log.info("VTAnalysis: submit_file: hash = %s " % (resource_hash))
-        params = {"apikey": self.api_token}
+        log.info(
+            "FortiSandbox Analysis: submit_file: hash = %s " %
+            (resource_hash))
+        params = {}
+        '''
+	"params": [
+            {
+                "file": "dGhpcyBpcyBhIHRlc3QhCg==",
+                "filename": b64encode("test.txt"),
+            }
+        ],
+	'''
         file_name = None
         if hasattr(stream, "name"):
             log.info("submitting file: fs.name: %s" % stream.name)
             file_name = os.path.basename(stream.name)
-        files = {'file': (file_name, open(file_name, 'rb'))} if file_name else {'file': (resource_hash, stream)}
-        response = self.session.post(self.url + 'scan', files=files, params=params)
-        log.debug("submit_file: response = %s" % response)
-        if response.status_code == 204:
-            raise VTAPIQUOTAREACHED()
-        return response.json()
-
-    def rescan_hash(self, resource_hash):
-        log.info("rescan_hash: resource_hash = %s" % resource_hash)
-        params = {"apikey": self.api_token}
-        if resource_hash:
-            params["resource"] = resource_hash
-        else:
-            raise Exception("No resources provided")
-        headers = {
-            "Accept-Encoding": "gzip, deflate",
-            "User-Agent": "gzip,  cb-fortisandbox-connector/1.0"
-        }
-        response = self.session.post(self.url + "rescan", params=params, headers=headers)
-        if response.status_code == 204:
-            raise VTAPIQUOTAREACHED()
-        log.debug("Rescan hash: response = %s" % response)
-        return response.json()
+        params['filename'] = b64encode(file_name)
+        params['file'] = b64encode(stream.read())
+        response = api_fortisandbox.handle_request(
+            host,
+            session=self.session,
+            sid=self.sid,
+            params=params,
+            request_type='file_upload')
+        log.debug("sub_file: response = %s" % response)
+        return response
 
     def get_report(self, resource_hash=None, batch=None):
         log.info("get_report: resource_hash = %s" % resource_hash)
-        params = {"apikey": self.api_token}
+        params = {"ctype": "md5","url":"/scan/results/file"}
+        '''
+	params": [
+            {
+                "sha256": "90877c1f6e7c97fb11249dc28dd16a3a3ddfac935d4f38c69307a71d96c8ef45",
+		"ctype": "sha256"
+            }
+        ],
+	'''
         if resource_hash:
-            params["resource"] = resource_hash
-        elif batch:
-            params['resource'] = ",".join(batch)
+            params["checksum"] = resource_hash
         else:
             raise Exception("No resources provided")
-        headers = {
-            "Accept-Encoding": "gzip, deflate",
-            "User-Agent": "gzip,  cb-fortisandbox-connector/1.0"
-        }
-        response = self.session.get(self.url + "report",
-                                    params=params, headers=headers)
-        if response.status_code == 204:
-            raise VTAPIQUOTAREACHED()
-        log.debug("get_report: response = %s " % response)
-        return response.json()
+        response = api_fortisandbox.handle_request(
+            host=self.host,
+            session=self.session,
+            sid=self.sid,
+            params=params,
+            request_type="get_file_verdict")
+        return response
--- cbopensource/connectors/fortisandbox/test.py
+++ cbopensource/connectors/fortisandbox/test.py
@@ -4,12 +4,12 @@ from requests import Session
 from os import environ as env
 import sys
 
-if __name__=="__main__":
+if __name__ == "__main__":
     cmd = sys.argv[1]
     vt = VtClient(Session(), env['VT_API_KEY'])
     if (cmd == "report"):
         print vt.get_report(resource_hash=sys.argv[2])
     elif (cmd == "scan"):
-        print vt.submit_file(stream=open(sys.argv[2],"rb"))
+        print vt.submit_file(stream=open(sys.argv[2], "rb"))
     elif (cmd == "rescan"):
         print vt.rescan_hash(resource_hash=sys.argv[2])
